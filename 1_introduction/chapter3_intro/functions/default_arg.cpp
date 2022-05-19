83b73beb31ca949ad26985e9cc73e26cb6939453
Yasmini Gyawali <gyawaliyasmini246@gmail.com>
2022-05-19 14:12:30 +0545

    default argument

---
+++ 1_introduction/chapter3_intro/functions/default_arg.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int sum(int x, int y, int z)
+{
+    return x+y+z;
+}
+int sum(int a, int b)
+{
+    return a+b;
+}
+int main()
+{
+    
+    cout<<"the sum is:"<<sum()
+    ;
+}
