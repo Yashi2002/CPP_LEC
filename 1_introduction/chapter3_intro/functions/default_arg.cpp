d9af873892cb38c522e35ceb1346c2e4586a4c2d
Yasmini Gyawali <gyawaliyasmini246@gmail.com>
2022-05-19 19:38:02 +0545

    Array Sorting

--- 1_introduction/chapter3_intro/functions/default_arg.cpp
+++ 1_introduction/chapter3_intro/functions/default_arg.cpp
@@ -4,13 +4,13 @@ int sum(int x, int y, int z)
 {
     return x+y+z;
 }
-int sum(int a, int b)
+int sum(int a=1, int b=2)
 {
     return a+b;
 }
 int main()
 {
     
-    cout<<"the sum is:"<<sum()
-    ;
+    cout<<"the sum is: "<< sum();
+        
 }
